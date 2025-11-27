1229c74d8c68020f5440e1332571ce3f24704887
SerwayLee <66809423+SerwayLee@users.noreply.github.com>
2025-11-27 20:28:54 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
-const { server, app } = require("./app");
+const { server } = require("./app");
 
 const PORT = process.env.PORT || 3000;
-const path = require('path');
 
-server.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server listening on http://localhost:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server listening on port ${PORT}`);
 });
