e70afaa5fbfabebd2512eb991362cb0f398a64f6
SerwayLee <66809423+SerwayLee@users.noreply.github.com>
2025-11-27 20:19:27 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const { server, app } = require("./app");
 
 const PORT = process.env.PORT || 3000;
 const path = require('path');
-app.get("/", (_req, res) => res.send("Chess server running"));
 
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
