9d67dc0772eaa06433b7314c9fe70c40b181bd0f
SerwayLee <66809423+SerwayLee@users.noreply.github.com>
2025-11-27 20:42:50 +0900

    Update LoginRegisterPage.js

--- src/components/LoginRegisterPage.js
+++ src/components/LoginRegisterPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const API_BASE = process.env.REACT_APP_SERVER_URL || "http://localhost:4000";
+const API_BASE = process.env.REACT_APP_SERVER_URL || "http://localhost:3000";
 
 const LoginRegisterPage = () => {
   const navigate = useNavigate();
